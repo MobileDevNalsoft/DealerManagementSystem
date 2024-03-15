2866500a0621c511af8b5521e21948bd505bd6cf
madhankumar-ch <chiluverimadhankumarnetha@gmail.com>
2024-03-15 11:43:35 +0530

    ui in progress

--- lib/views/homeview.dart
+++ lib/views/homeview.dart
@@ -11,6 +11,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeView extends State<HomeView> {
+  FocusNode focus = FocusNode();
   TextEditingController txcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -34,117 +35,168 @@ class _HomeView extends State<HomeView> {
                 ),
                 fit: BoxFit.cover),
           ),
-          child: Center(
-            child: SizedBox(
-              width: size.width * 0.3,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              Consumer<HomeProvider>(
+                builder: (context, provider, child) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SearchableDropDown(
+                          size: size,
+                          hint: 'Location',
+                          items: [
+                            'Location 1',
+                            'Location 2',
+                            'Location 3',
+                            'Location 4',
+                            'Location 5'
+                          ],
+                          focus: focus,
+                          txcontroller: txcontroller,
+                          provider: provider),
+                      CustomDataCard(
+                          size: size, hint: 'Vehicle Registration Number'),
+                      SearchableDropDown(
+                          size: size,
+                          hint: 'Customer',
+                          items: [
+                            'Customer 1',
+                            'Customer 2',
+                            'Customer 3',
+                            'Customer 4',
+                            'Customer 5'
+                          ],
+                          focus: focus,
+                          txcontroller: txcontroller,
+                          provider: provider),
+                      CustomDataCard(size: size, hint: 'Schedule Date'),
+                      CustomDataCard(size: size, hint: 'KMS'),
+                    ],
+                  );
+                },
+              ),
+              Row(
                 children: [
                   SizedBox(
-                    height: size.height * 0.1,
+                    width: size.width * 0.583,
                   ),
-                  Consumer<HomeProvider>(
-                    builder: (context, provider, child) {
-                      return Column(
-                        children: [
-                          TypeAheadField(
-                            builder: (context, controller, focusNode) {
-                              txcontroller = controller;
-                              return SizedBox(
-                                height: size.height * 0.063,
-                                child: Card(
-                                  color: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(5)),
-                                  child: Center(
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(
-                                          bottom: 5.0, left: 14),
-                                      child: TextFormField(
-                                        style: TextStyle(color: Colors.black54),
-                                        decoration: InputDecoration(
-                                          border: InputBorder
-                                              .none, // Removes all borders
-                                        ),
-                                        controller: controller,
-                                        focusNode: focusNode,
-                                        autofocus: true,
-                                        validator: (value) => value!.isEmpty
-                                            ? 'Please select a value'
-                                            : [
-                                                'Location 1',
-                                                'Location 2',
-                                                'Location 3',
-                                                'Location 4',
-                                                'Location 5'
-                                              ].any((element) =>
-                                                    element.toLowerCase() ==
-                                                    value.toLowerCase())
-                                                ? null
-                                                : 'Please select a valid location',
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                            suggestionsCallback: (pattern) {
-                              return [
-                                'Location 1',
-                                'Location 2',
-                                'Location 3',
-                                'Location 4',
-                                'Location 5'
-                              ]
-                                  .where((item) => item
-                                      .toLowerCase()
-                                      .contains(pattern.toLowerCase()))
-                                  .toList();
-                            },
-                            itemBuilder: (context, suggestion) => ListTile(
-                              title: Text(suggestion),
-                            ),
-                            onSelected: (suggestion) {
-                              txcontroller.text = suggestion;
-                            },
-                          ),
-                          CustomDataCard(size: size, content: 'AP09AS1234'),
-                          CustomDataCard(size: size, content: 'Schedule Date'),
-                          CustomDataCard(size: size, content: 'KMS'),
-                        ],
-                      );
-                    },
-                  ),
-                  Row(
-                    children: [
-                      Expanded(child: SizedBox()),
-                      ElevatedButton(onPressed: () {}, child: Text('view more'))
-                    ],
-                  )
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'view more',
+                        style: TextStyle(color: Color.fromRGBO(40, 83, 235, 1)),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: Size(80.0, 20.0),
+                          padding: EdgeInsets.zero,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5))))
                 ],
               ),
-            ),
+              ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'next',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(70.0, 35.0),
+                      padding: EdgeInsets.zero,
+                      backgroundColor: Color.fromRGBO(40, 83, 235, 1),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5))))
+            ],
           ),
         ),
       ),
     );
   }
 
-  Widget CustomDataCard({required Size size, required String content}) {
+  Widget SearchableDropDown(
+      {required size,
+      required hint,
+      required List<String> items,
+      required FocusNode focus,
+      required TextEditingController txcontroller,
+      required HomeProvider provider}) {
+    return SizedBox(
+      height: size.height * 0.063,
+      width: size.width * 0.3,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        child: Row(
+          children: [
+            SizedBox(
+              height: size.height * 0.05,
+              width: size.width * 0.25,
+              child: TypeAheadField(
+                builder: (context, controller, focusNode) {
+                  focus = focusNode;
+                  txcontroller = controller;
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 14, top: 15),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: hint,
+                        hintStyle: TextStyle(
+                            color: Colors.black54,
+                            fontWeight: FontWeight.normal),
+                        border: InputBorder.none, // Removes all borders
+                      ),
+                      controller: controller,
+                      focusNode: focusNode,
+                    ),
+                  );
+                },
+                suggestionsCallback: (pattern) {
+                  return items
+                      .where((item) =>
+                          item.toLowerCase().contains(pattern.toLowerCase()))
+                      .toList();
+                },
+                itemBuilder: (context, suggestion) => ListTile(
+                  title: Text(suggestion),
+                ),
+                onSelected: (suggestion) {
+                  txcontroller.text = suggestion;
+                  focus.unfocus();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget CustomDataCard(
+      {required Size size,
+      required String hint,
+      TextEditingController? txcontroller}) {
     return SizedBox(
       height: size.height * 0.06,
       width: size.width * 0.3,
       child: Card(
         color: Colors.white.withOpacity(1),
         child: Padding(
-          padding: const EdgeInsets.only(top: 8.0, left: 14),
-          child: Text(
-            content,
-            style:
-                TextStyle(fontWeight: FontWeight.normal, color: Colors.black54),
-          ),
-        ),
+            padding: const EdgeInsets.only(
+              top: 15,
+              left: 13,
+            ),
+            child: TextFormField(
+              controller: txcontroller,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: hint,
+                  hintStyle: TextStyle(
+                      color: Colors.black54, fontWeight: FontWeight.normal)),
+            )),
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(5))),
       ),
