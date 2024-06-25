c6f611f8d3dc099057c75aeeb3dbb57cda33f460
saisanjit <saisanjit.d@gmail.com>
2024-06-25 12:30:41 +0530

    updates

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,17 +6,23 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
 import 'package:pointer_interceptor_web/pointer_interceptor_web.dart';
 import 'package:rive_common/rive_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FlutterImageCompressWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
   PointerInterceptorWeb.registerWith(registrar);
   RivePlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
