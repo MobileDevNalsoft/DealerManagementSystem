bcc6f76e91874ccf4e731895e3cb4ab6a9881983
mobiledevnalsoft <mobiledevnalsoft@gmail.com>
2024-07-16 10:20:32 +0530

    added draggable sheet

---
+++ lib/views/custom_widgets/draggable_sheet.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DraggableSheet extends StatefulWidget {
+  final Widget child;
+  const DraggableSheet({super.key, required this.child});
+
+  @override
+  State<DraggableSheet> createState() => _DraggableSheetState();
+}
+
+class _DraggableSheetState extends State<DraggableSheet> {
+  final sheet = GlobalKey();
+  final contoller = DraggableScrollableController();
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return DraggableScrollableSheet(
+          builder: (context, scrollController) {
+            return DecoratedBox(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.yellow,
+                        blurRadius: 10,
+                        spreadRadius: 1,
+                        offset: Offset(0, 1),
+                      )
+                    ],
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(22),
+                        topRight: Radius.circular(22))));
+          },
+        );
+      },
+    );
+  }
+}
